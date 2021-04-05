841c305b1d7e602362dca10d6938591a593b96d6
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2021-04-05 02:13:51 -0400

    Update var suggest type

--- CodeSnifferContrib/Sniffs/Commenting/FunctionCommentSniff.php
+++ CodeSnifferContrib/Sniffs/Commenting/FunctionCommentSniff.php
@@ -13,6 +13,25 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
 {
 
     /**
+     * An array of variable types for param/var we will check.
+     *
+     * @var string[]
+     */
+    public static $allowedTypes = [
+        'array',
+        'bool',
+        'boolean',
+        'float',
+        'int',
+        'integer',
+        'mixed',
+        'object',
+        'string',
+        'resource',
+        'callable',
+    ];
+
+    /**
      * The current PHP version.
      *
      * @var integer
@@ -72,7 +91,7 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
                 $typeNames      = explode('|', $returnType);
                 $suggestedNames = [];
                 foreach ($typeNames as $i => $typeName) {
-                    $suggestedName = Common::suggestType($typeName);
+                    $suggestedName = self::suggestType($typeName);
                     if (in_array($suggestedName, $suggestedNames, true) === false) {
                         $suggestedNames[] = $suggestedName;
                     }
@@ -377,7 +396,7 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
                     $typeName = substr($typeName, 1);
                 }
 
-                $suggestedName        = Common::suggestType($typeName);
+                $suggestedName        = self::suggestType($typeName);
                 $suggestedTypeNames[] = $suggestedName;
 
                 if (count($typeNames) > 1) {
@@ -689,5 +708,79 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
 
     }//end checkSpacingAfterParamName()
 
+
+
+    /**
+     * Returns a valid variable type for param/var tags.
+     *
+     * If type is not one of the standard types, it must be a custom type.
+     * Returns the correct type name suggestion if type name is invalid.
+     *
+     * @param string $varType The variable type to process.
+     *
+     * @return string
+     */
+    public static function suggestType($varType)
+    {
+        if ($varType === '') {
+            return '';
+        }
+
+        if (in_array($varType, self::$allowedTypes, true) === true) {
+            return $varType;
+        } else {
+            $lowerVarType = strtolower($varType);
+            switch ($lowerVarType) {
+                case 'bool':
+                case 'boolean':
+                    return 'bool';
+                case 'double':
+                case 'real':
+                case 'float':
+                    return 'float';
+                case 'int':
+                case 'integer':
+                    return 'int';
+                case 'array()':
+                case 'array':
+                    return 'array';
+            }//end switch
+
+            if (strpos($lowerVarType, 'array(') !== false) {
+                // Valid array declaration:
+                // array, array(type), array(type1 => type2).
+                $matches = [];
+                $pattern = '/^array\(\s*([^\s^=^>]*)(\s*=>\s*(.*))?\s*\)/i';
+                if (preg_match($pattern, $varType, $matches) !== 0) {
+                    $type1 = '';
+                    if (isset($matches[1]) === true) {
+                        $type1 = $matches[1];
+                    }
+
+                    $type2 = '';
+                    if (isset($matches[3]) === true) {
+                        $type2 = $matches[3];
+                    }
+
+                    $type1 = self::suggestType($type1);
+                    $type2 = self::suggestType($type2);
+                    if ($type2 !== '') {
+                        $type2 = ' => '.$type2;
+                    }
+
+                    return "array($type1$type2)";
+                } else {
+                    return 'array';
+                }//end if
+            } else if (in_array($lowerVarType, self::$allowedTypes, true) === true) {
+                // A valid type, but not lower cased.
+                return $lowerVarType;
+            } else {
+                // Must be a custom type name.
+                return $varType;
+            }//end if
+        }//end if
+
+    }//end suggestType()
 
 }//end class
